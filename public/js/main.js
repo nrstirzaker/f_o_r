30f08aa1930f45b373c4bd2e6ed88b085996940f
nrstirzaker <nrstirzaker.techblog@gmail.com>
2017-10-28 00:18:10 +0100

    getting page to submit

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-    $("signup").click(function(){
+    $("#signup").click(function(){
         $.post("api/add",
         {
-            email: $('email')
+            email: $('#email')
         },
         function(data, status){
             alert("Data: " + data + "\nStatus: " + status);
