6425ca3f8ee3c5406a1774b30daa152520a1f52a
nrstirzaker <nrstirzaker.techblog@gmail.com>
2017-10-28 00:54:36 +0100

    passing data correctly

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         $.ajax({
             url: "/api/add",
             method : "post",
-            data: JSON.stringify( { "email": $('#email').val() } ),
+            data: { "email": $('#email').val() },
             success: function(data, status){
                 $('#response').html( JSON.stringify( data ) );
             }
